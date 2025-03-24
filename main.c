8d85bc82af1a7dfd1d85eeb706088e4e0ac59433
William Torres <contatowilliamtorres@gmail.com>
2025-03-24 19:40:33 -0300

    feat: Adiciona funcionalidade para ler e printar valores digitados pelo usuário.

--- main.c
+++ main.c
@@ -3,5 +3,9 @@
 #include <string.h>
 
 int main(int argc, char **argv){
+  int i, j;
+  printf("Digite 2 valores: ");
+  scanf("%d %d", &i, &j);
+  printf("Os valores digitados: i:%d j:%d", i, j);
   return 0;
 }
